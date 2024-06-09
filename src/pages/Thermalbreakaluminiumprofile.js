642f5b81b495729ee17524b496cc8a26e8aaa285
Deep-Mavani <deepmavani16@gmail.com>
2024-06-09 17:14:52 +0530

    Changing the product description for all the products in the main product page

--- src/pages/Thermalbreakaluminiumprofile.js
+++ src/pages/Thermalbreakaluminiumprofile.js
@@ -9,8 +9,7 @@ function Thermalbreakaluminiumprofile() {
       mainImage={thermalBreakAluminiumProfileImage}
       mainAltText="Thermal Break Aluminium Profile"
       title="Thermal Break Aluminium Profile"
-      description="
-A thermal break aluminum profile is a type of window or door frame that includes a non-conductive material between the inner and outer aluminum sections to reduce heat transfer, improving energy efficiency and reducing condensation."
+      description="A thermal break aluminum profile is a type of window or door frame that includes a non-conductive material between the inner and outer aluminum sections to reduce heat transfer, improving energy efficiency and reducing condensation."
       productList={Thermalaluminiumlist}
     />
   )
