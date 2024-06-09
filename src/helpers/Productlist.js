25b17ad2816d69029beaf1609158a160deb269f9
Deep-Mavani <deepmavani16@gmail.com>
2024-06-09 17:07:08 +0530

    Changing the product images for all the products in the main product page

--- src/helpers/Productlist.js
+++ src/helpers/Productlist.js
@@ -1,46 +1,51 @@
-import product1 from '../assets/thermalhd.JPG';
+import thermalBreakProfileImage from '../assets/thermalhd.JPG';
+import thermalBreakAluminiumProfileImage from '../assets/thermal break aluminium profile/thermal-break-aluminium-profile.jpg';
+import aluminiumExtrusionImage from '../assets/aluminium extrusion/AE.jpg';
+import hvacImage from '../assets/hvac solutions/hvac.jpg';
+import powderCoatingImage from '../assets/powder coating/powdercoating.jpg';
+import woodenCoatingImage from '../assets/woodencoating.png';
 
 export const Productlist =[
     {
         id:1,
         head: " Thermal Break Profiles",
         para: "High-quality standard solutions always available for fast delivery of any length and required quantity.",
-        image: product1,
+        image: thermalBreakProfileImage,
         url: '/thermalbreakprofile' ,
     },
     {
         id:2,
         head: " Thermal Break Aluminium Profile",
         para: "High-quality standard solutions always available for fast delivery of any length and required quantity.",
-        image: product1, 
+        image: thermalBreakAluminiumProfileImage,
         url: '/thermalbreakaluminiumprofile',
     },
     {
         id:3,
         head: "HVAC Solutions ",
         para: "High-quality standard solutions always available for fast delivery of any length and required quantity.",
-        image: product1, 
+        image: hvacImage,
         url: '/hvacsolution',
     },
     {
         id:4,
         head: " Aluminium Extrusion ",
         para: "High-quality standard solutions always available for fast delivery of any length and required quantity.",
-        image: product1, 
+        image: aluminiumExtrusionImage,
         url: '/aluminiumextrusion',
     },
     {
         id:5,
         head: " Powder Coating ",
         para: "High-quality standard solutions always available for fast delivery of any length and required quantity.",
-        image: product1, 
+        image: powderCoatingImage,
         url: '/powdercoating',
     },
     {
         id:6,
         head: " Wooden Coating ",
         para: "High-quality standard solutions always available for fast delivery of any length and required quantity.",
-        image: product1, 
+        image: woodenCoatingImage,
         url: '/woodencoating',
     }
 ];
